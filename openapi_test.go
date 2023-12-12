75c96e0bba7f50224943a90692295b2c821d75b1
dan-j <5727701+dan-j@users.noreply.github.com>
2023-12-12 11:49:55 +0000

    chore(openapi): add comments to try and illustrate why I'm doing what I'm doing

--- openapi_test.go
+++ openapi_test.go
@@ -11,22 +11,11 @@ var openapiBundle embed.FS
 func ExampleCompile() {
 	c := NewCompiler()
 
-	oas31SchemaReader, err := openapiBundle.Open("testdata/openapi/openapi_3.1.schema.json")
-	if err != nil {
-		panic(err)
-	}
+	// we want to validate OpenAPI 3.1 specifications against its meta schema. As I understand it
+	// this is the correct way to add support for custom vocabularies?
+	registerExtension(c)
 
-	if err := c.AddResource("https://spec.openapis.org/oas/3.1/schema/2022-10-07", oas31SchemaReader); err != nil {
-		panic(err)
-	}
-
-	oas31Schema, err := c.Compile("https://spec.openapis.org/oas/3.1/schema/2022-10-07")
-	if err != nil {
-		panic(err)
-	}
-
-	c.RegisterExtension("openapi_3.1", oas31Schema, &oas3SchemaCompiler{})
-
+	// load the spec, this works as expected
 	validSpecReader, err := openapiBundle.Open("testdata/openapi/valid-spec.json")
 	if err != nil {
 		panic(err)
@@ -43,6 +32,15 @@ func ExampleCompile() {
 
 	fmt.Println("Compiled: valid-spec.json with extensions:", len(compiled.Extensions))
 
+	// now we receive a request with some data which is associated to one of the operations defined
+	// in the spec. We want to validate the request data against that operation, which we'll say
+	// here is defined by the Foo schema, so we want to compile this into a proper Schema so that we
+	// can call `validSubSchema.Validate()`.
+	//
+	// What's happening is it's compiling the referenced schema against the root of the openapi meta
+	// schema. I'd have expected it to compile against the schema located at:
+	//
+	//   https://spec.openapis.org/oas/3.1/schema/2022-10-07#/properties/components/properties/schemas/additionalProperties
 	validSubSchema, err := c.Compile("valid-spec.json#/components/schemas/Foo")
 	if err != nil {
 		fmt.Println("ERROR: failed to compile subschema \"valid-spec.json#/components/schemas/Foo\":", err.Error())
@@ -50,6 +48,9 @@ func ExampleCompile() {
 		fmt.Println("Compiled: valid-spec.json#/components/schemas/Foo with extensions:", len(validSubSchema.Extensions))
 	}
 
+	// ---
+	// now we load an invalid spec, this kind of works as expected, however it would be nice if we
+	// could still compile the subschema and only care about those errors (if there are any).
 	invalidSpecReader, err := openapiBundle.Open("testdata/openapi/invalid-spec.json")
 	if err != nil {
 		panic(err)
@@ -61,12 +62,15 @@ func ExampleCompile() {
 
 	_, err = c.Compile("invalid-spec.json")
 	if err != nil {
+		// expected
 		fmt.Println("failed to compile invalid-spec.json")
 	}
 
 	// invalid spec but valid subSchema
 	_, err = c.Compile("invalid-spec.json#/components/schemas/Foo")
 	if err != nil {
+		// this fails for the same reason above, it's compiling against the root of the meta schema
+		// and not the schema pointed at /properties/components/properties/schemas/additionalProperties
 		fmt.Println("ERROR: failed to compile subschema \"invalid-spec.json#/components/schemas/Foo\":", err.Error())
 	} else {
 		fmt.Println("Compiled: invalid-spec.json#/components/schemas/Foo with extensions:", len(validSubSchema.Extensions))
@@ -75,6 +79,7 @@ func ExampleCompile() {
 	// invalid spec and invalid subSchema
 	_, err = c.Compile("invalid-spec.json#/components/schemas/FooInvalid")
 	if err != nil {
+		// This fails as expected :)
 		fmt.Println("failed to compile subschema \"invalid-spec.json#/components/schemas/FooInvalid\"")
 	} else {
 		fmt.Println("ERROR: unexpectedly compiled invalid-spec.json#/components/schemas/FooInvalid with extensions:", len(validSubSchema.Extensions))
@@ -86,6 +91,24 @@ func ExampleCompile() {
 	// failed to compile invalid-spec.json
 	// Compiled: invalid-spec.json#/components/schemas/Foo with extensions: 1
 	// failed to compile subschema "invalid-spec.json#/components/schemas/FooInvalid"
+}
+
+func registerExtension(c *Compiler) {
+	oas31SchemaReader, err := openapiBundle.Open("testdata/openapi/openapi_3.1.schema.json")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.AddResource("https://spec.openapis.org/oas/3.1/schema/2022-10-07", oas31SchemaReader); err != nil {
+		panic(err)
+	}
+
+	oas31Schema, err := c.Compile("https://spec.openapis.org/oas/3.1/schema/2022-10-07")
+	if err != nil {
+		panic(err)
+	}
+
+	c.RegisterExtension("openapi_3.1", oas31Schema, &oas3SchemaCompiler{})
 }
 
 type oas3SchemaCompiler struct{}
